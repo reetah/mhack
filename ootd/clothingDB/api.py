395ef0b7b9b2666419858205e8c98e0308ecd0fe
bzfisher <me@bfisher.ca>
2014-01-18 04:03:32 -0500

    added api support for user; still need to fix display of clothing attribute in user

--- ootd/clothingDB/api.py
+++ ootd/clothingDB/api.py
@@ -1,4 +1,6 @@
+from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
+from tastypie import fields
 from clothingDB.models import Clothing, User
 
 class ClothingResource(ModelResource):
@@ -7,6 +9,8 @@ class ClothingResource(ModelResource):
 		resource_name = 'clothing'
 
 class UserResource(ModelResource):
+	clothes = fields.ManyToManyField(ClothingResource, 'clothing')
 	class Meta:
 		queryset = User.objects.all()
 		resource_name = 'user'
+		authorization= Authorization()
