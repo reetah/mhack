37a2b61a3895d3bb5bc7f2743b59a09b63f38223
bzfisher <me@bfisher.ca>
2014-01-18 19:50:27 -0500

    kai is stupid

--- js/index.js
+++ js/index.js
@@ -207,7 +207,6 @@ var showAdd = function() {
     $("#modal").slideDown();
 }
 
-<<<<<<< HEAD
 
 var addThis = function() {
     console.log("add this cloth");
@@ -224,14 +223,18 @@ var addThis = function() {
     item.url = "testin.html";
     item.weatherIndex = 20;
 
+    var data1 = JSON.stringify({
+    "name": "This will prbbly be my lst post.",
+});
 
     $.ajax({
         url: 'http://localhost:8000/api/clothing/',
         type: 'POST',
-        data: item,
-        dataType: "json",
-
-        ContentType: 'application/json; charset=utf-8',
+        contentType: 'application/json',
+        data: JSON.stringify(item),
+        dataType: 'json',
+        processData: false,
+       
         success: function(data) {
             console.log(data);
         },
@@ -240,7 +243,6 @@ var addThis = function() {
 
 }
 //binder
-=======
 //binder
 
 
@@ -262,4 +264,3 @@ var dropLoadType = function(type) {
     }
 
 }
->>>>>>> 7a9f12d739233ac53ae236aee45d8bbdbff0cbde
